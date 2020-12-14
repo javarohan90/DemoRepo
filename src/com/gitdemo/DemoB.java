5e7d466e2b7da8434dc0469546c49301117c5278
Rohan Ghatage <rohan.ghatage@ecotechservices.com>
2020-12-14 14:14:29 +0530

    my git commit

---
+++ src/com/gitdemo/DemoB.java
@@ -0,0 +1,11 @@
+package com.gitdemo;
+
+public class DemoB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My new class for 2nd commit");
+
+	}
+
+}
