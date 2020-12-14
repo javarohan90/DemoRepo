28857e7f2bd289296f255b0d7515e95b0ca052d6
Rohan Ghatage <rohan.ghatage@ecotechservices.com>
2020-12-14 14:22:03 +0530

    my third git commit

--- src/com/gitdemo/DemoA.java
+++ src/com/gitdemo/DemoA.java
@@ -5,6 +5,7 @@ public class DemoA {
 	public static void main(String[] args) {
 
 		System.out.println("Demo for Git Hub");
+		System.out.println("Git updating");
 
 	}
 
---
+++ src/com/gitdemo/DemoC.java
@@ -0,0 +1,9 @@
+package com.gitdemo;
+
+public class DemoC {
+	public void testM1()
+	{
+		System.out.println("This is my test method");
+	}
+
+}
